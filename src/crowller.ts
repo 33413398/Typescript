065fb354ba056912d672abadcc405e10a3dfa609
haowei <haowei_web@163.com>
2021-05-20 23:18:55 +0800

    配置函数

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,3 +1,14 @@
+import superagent from 'superagent'
 class crowller {
   private url='http://h33413398.host3v.com/';
+  // 获取数据
+  private rowHtml = ''
+  // 类初始化就会触发构造器
+  constructor(){
+    this.getHtml()
+  }
+  async getHtml() {
+    const result = await superagent.get(this.url);
+    console.log(result);
+  }
 }
