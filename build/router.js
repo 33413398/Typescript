fb94bc2fbfc6396acdb008283eaa7423e469de58
haowei <haowei_web@163.com>
2021-06-06 22:32:53 +0800

    完成开发

--- build/router.js
+++ build/router.js
@@ -5,19 +5,60 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = require("express");
 var crowller_1 = __importDefault(require("./crowller"));
+// node文件操作
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
 var util_1 = require("./util");
 // 实例化路由
 var router = express_1.Router();
-// 创建get请求
-router.get('/', function (req, res) {
-    console.log(req.body);
-    res.send('helo!');
+// 创建登录请求
+router.post('/login', function (req, res) {
+    if (req.body.password == 123) {
+        if (req.session) {
+            req.session.login = true;
+        }
+        res.json(util_1.getResponseData(true));
+    }
+    else {
+        res.json(util_1.getResponseData(false, '密码错误，登录失败！'));
+    }
+});
+// 是否已登录
+router.get('/islogin', function (req, res) {
+    var isLogin = req.session ? req.session.login : false;
+    if (isLogin) {
+        res.json(util_1.getResponseData(true));
+    }
+    else {
+        res.json(util_1.getResponseData(false, '用户未登录请先登录！'));
+    }
+});
+// 退出登录
+router.get('/outlogin', function (req, res) {
+    if (req.session) {
+        req.session.login = false;
+    }
+    res.json(util_1.getResponseData(true));
 });
 // 刷新网页爬取数据
 router.get('/getData', function (req, res) {
     // 实例化类
-    var htmlData = new crowller_1.default();
+    new crowller_1.default();
     res.json(util_1.getResponseData(true));
+});
+// 将数据展示给页面
+router.get('/backData', function (req, res) {
+    var filePath = path_1.default.join(__dirname, '../data/dest.json');
+    // 文件内容
+    var fileContent = {};
+    if (fs_1.default.existsSync(filePath)) {
+        // 文件存在
+        fileContent = JSON.parse(fs_1.default.readFileSync(filePath, 'utf-8'));
+    }
+    else {
+        res.json();
+    }
+    res.json(util_1.getResponseData(fileContent));
 });
 // 导出
 exports.default = router;
--- src/router.ts
+++ src/router.ts
@@ -1,21 +1,67 @@
 import {Router,Request,Response} from 'express'
 import Crwller from './crowller'
+// node文件操作
+import fs from 'fs'
+import path from 'path'
 import {getResponseData} from './util'
 
 // 实例化路由
 const router = Router()
-
-// 创建get请求
-router.get('/',(req:Request,res:Response)=>{
-  console.log(req.body);
-  
-  res.send('helo!')
+// 数据类型
+interface Item {
+  title: string;
+  count: number;
+}
+// 内容类型
+interface Content {
+  // propName任意变量
+  [propName:number]:Item[]
+}
+// 创建登录请求
+router.post('/login',(req:Request,res:Response)=>{
+  if(req.body.password==123) {
+    if(req.session) {
+      req.session.login = true
+    }
+    res.json(getResponseData(true))
+  }else {
+    res.json(getResponseData(false,'密码错误，登录失败！'))
+  }
+})
+// 是否已登录
+router.get('/islogin',(req:Request,res:Response)=>{
+  const isLogin = req.session?req.session.login:false
+  if(isLogin) {
+    res.json(getResponseData(true))
+  }else {
+    res.json(getResponseData(false,'用户未登录请先登录！'))
+  }
+})
+// 退出登录
+router.get('/outlogin',(req:Request,res:Response)=>{
+  if(req.session) {
+    req.session.login = false
+  }
+  res.json(getResponseData(true))
 })
 // 刷新网页爬取数据
 router.get('/getData',(req:Request,res:Response)=>{
   // 实例化类
-    let htmlData =new Crwller();
+    new Crwller();
     res.json(getResponseData(true))
+})
+// 将数据展示给页面
+router.get('/backData',(req:Request,res:Response)=>{
+  const filePath = path.join(__dirname,'../data/dest.json')
+    // 文件内容
+    let fileContent:Content = {}
+    if(fs.existsSync(filePath)) {
+      // 文件存在
+      fileContent=JSON.parse(fs.readFileSync(filePath,'utf-8'))
+    }else {
+      res.json()
+    }
+    res.json(getResponseData(fileContent))
 })
 
 // 导出
